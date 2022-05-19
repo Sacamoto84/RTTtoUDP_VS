1fc7b18c77abbcbc0fbf10cd8004c18906a161b6
Ivan <zorromen@mail.ru>
2022-05-19 22:23:23 +0300

    +1

--- MinimalisticTelnet/Program.cs
+++ MinimalisticTelnet/Program.cs
@@ -13,35 +13,169 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
 
 namespace MinimalisticTelnet
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+
+
+        private static bool ConsoleVisible = false;
+        private const int HIDE = 0;
+        private const int SHOW = 9;
+        private const int MF_BYCOMMAND = 0;
+        private const int SC_CLOSE = 0xF060;
+
+
+        private static IntPtr ConsoleWindow = GetConsoleWindow();
+  
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        static void ToggleWindow(bool visible)
         {
+            ConsoleVisible = visible;
+            ShowWindow(ConsoleWindow, visible ? SHOW : HIDE);
+        }
+
+        
+
+        private static void Adder()
+        {
+
             UTF8Encoding utf8 = new UTF8Encoding();
 
             IPEndPoint ip = new IPEndPoint(IPAddress.Parse("192.168.0.255"), 8888);
-            byte[] data = new byte[2048*16];
+            byte[] data = new byte[2048 * 32];
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-            TelnetConnection tc = new TelnetConnection("127.0.0.1", 19021);
-            string s;// = tc.Login("","",1000);
+            TelnetConnection tc = null;
+            int count = 0;
 
-            // while connected
-            while (tc.IsConnected)
+            while (true)
             {
-                String decodedString = utf8.GetString(tc.Read());
-                Console.Write(decodedString);
-                if (decodedString != "")
+                try
                 {
-                    data = Encoding.UTF8.GetBytes(decodedString);
+
+                    data = Encoding.UTF8.GetBytes("\u001b[01;38;05;107m*** Подключение к Telnet ***\u001b[0m");
                     server.SendTo(data, data.Length, SocketFlags.None, ip);
-                }  
+                    Console.WriteLine("\u001b[01;38;05;107m***Подключение к Telnet ***\u001b[0m");
+                    tc = new TelnetConnection("localhost", 19021);
+                }
+                catch (Exception ex)
+                {
+                    if (tc != null)
+                    {
+                        tc.tcpSocket.Close();
+                    }
+                    Console.WriteLine("\u001b[38;05;232;48;05;196m***Ошибка подключения к Telnet***\u001b[0m");
+                    data = Encoding.UTF8.GetBytes("\u001b[38;05;232;48;05;196m***Ошибка подключения к Telnet***\u001b[0m");
+                    server.SendTo(data, data.Length, SocketFlags.None, ip);
+                }
+
+                if (tc != null)
+                {
+                    while (tc.IsConnected)
+                    {
+                        //Проверяем подключение
+                        if (tc.checkConnect() == false)
+                        {
+                            data = Encoding.UTF8.GetBytes("\x1B[38;5;125m*** Error Telnet Server Отключет ***");
+                            server.SendTo(data, data.Length, SocketFlags.None, ip);
+                            break;
+                        }
+
+                        byte[] buf = tc.Read();
+
+                        //Console.Write("\x1B[38;5;125mfffff");
+
+                        if (buf != null)
+                        {
+                            if (buf.Length > 0)
+                            {
+                                count++;
+                                String decodedString = utf8.GetString(buf);
+                                Console.Write($"{count} : {decodedString}");
+                                server.SendTo(buf, buf.Length, SocketFlags.None, ip);
+                            }
+                        }
+
+
+
+                    }
+                }
+
+                //data = Encoding.UTF8.GetBytes("***DISCONNECTED");
+                //server.SendTo(data, data.Length, SocketFlags.None, ip);
+                //Console.WriteLine("***DISCONNECTED");
             }
-            Console.WriteLine("***DISCONNECTED");
+
             server.Close();
+
+
+
+
         }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RTT UDP v1.0");
+
+            ShowWindow(GetConsoleWindow(), 0);
+
+            NotifyIcon icon = new NotifyIcon();
+            icon.Icon = new System.Drawing.Icon("altyo.ico");
+            icon.Visible = true;
+            icon.Text = "Свернутое консольное приложение";
+            icon.Click += Ni_Click;
+
+            Thread t = new Thread(Adder);
+            t.Name = "Thread1";
+            t.Start();
+
+
+            Application.Run();
+
+
+
+            //
+
+           
+
+
+
+
+        }
+
+
+        static int Show = 0;
+        private static void Ni_Click(object sender, EventArgs e)
+        {
+            if (Show == 0)
+            {
+                Show = 1;
+                ShowWindow(GetConsoleWindow(), 1);
+            }
+            else
+            {
+                Show = 0;
+                ShowWindow(GetConsoleWindow(), 0);
+            }
+            
+            
+        }
+
+
     }
 }
--- MinimalisticTelnet/TelnetInterface.cs
+++ MinimalisticTelnet/TelnetInterface.cs
@@ -12,7 +12,8 @@ using System.Net.Sockets;
 
 namespace MinimalisticTelnet
 {
-    enum Verbs {
+    enum Verbs
+    {
         WILL = 251,
         WONT = 252,
         DO = 253,
@@ -27,9 +28,9 @@ namespace MinimalisticTelnet
 
     class TelnetConnection
     {
-        TcpClient tcpSocket;
+        public TcpClient tcpSocket;
 
-        int TimeOutMs = 100;
+        int TimeOutMs = 10;
 
         public TelnetConnection(string Hostname, int Port)
         {
@@ -37,7 +38,6 @@ namespace MinimalisticTelnet
 
         }
 
-   
         public void WriteLine(string cmd)
         {
             Write(cmd + "\n");
@@ -46,20 +46,45 @@ namespace MinimalisticTelnet
         public void Write(string cmd)
         {
             if (!tcpSocket.Connected) return;
-            byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd.Replace("\0xFF","\0xFF\0xFF"));
+            byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd.Replace("\0xFF", "\0xFF\0xFF"));
             tcpSocket.GetStream().Write(buf, 0, buf.Length);
         }
+
+        //Проверка соединения
+        public bool checkConnect()
+        {
+
+            if (tcpSocket.Client.Poll(0, SelectMode.SelectRead))
+            {
+                byte[] buff = new byte[1];
+                if (tcpSocket.Client.Receive(buff, SocketFlags.Peek) == 0)
+                {
+                    // Client disconnected
+                    Console.WriteLine("Jlink Telnet отключен");
+                    tcpSocket.Close();
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 
         public Byte[] Read()
         {
             if (!tcpSocket.Connected) return null;
-            //StringBuilder sb=new StringBuilder();
+
             List<byte> list = new List<byte>();
             do
             {
+                if (!tcpSocket.Connected)
+                    return list.ToArray();
+
                 ParseTelnet(list);
                 System.Threading.Thread.Sleep(TimeOutMs);
+
             } while (tcpSocket.Available > 0);
+
             return list.ToArray();//sb.ToString();
         }
 
@@ -71,7 +96,7 @@ namespace MinimalisticTelnet
         //void ParseTelnet(StringBuilder sb)
         void ParseTelnet(List<byte> list)
         {
-        
+
 
             while (tcpSocket.Available > 0)
             {
@@ -80,10 +105,10 @@ namespace MinimalisticTelnet
                 //if ((input == 0xd0) || (input == 0xd1)) {   
                 //    input = (UInt16)(input << 8) | (UInt16)(tcpSocket.GetStream().ReadByte());         
                 //}
-          
+
                 switch (input)
                 {
-                    case -1 :
+                    case -1:
                         break;
                     case (int)Verbs.IAC:
                         // interpret as command
@@ -98,7 +123,7 @@ namespace MinimalisticTelnet
 
                                 //sb.Append(inputverb);
                                 break;
-                            case (int)Verbs.DO: 
+                            case (int)Verbs.DO:
                             case (int)Verbs.DONT:
                             case (int)Verbs.WILL:
                             case (int)Verbs.WONT:
@@ -106,10 +131,10 @@ namespace MinimalisticTelnet
                                 int inputoption = tcpSocket.GetStream().ReadByte();
                                 if (inputoption == -1) break;
                                 tcpSocket.GetStream().WriteByte((byte)Verbs.IAC);
-                                if (inputoption == (int)Options.SGA )
-                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WILL:(byte)Verbs.DO); 
+                                if (inputoption == (int)Options.SGA)
+                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WILL : (byte)Verbs.DO);
                                 else
-                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WONT : (byte)Verbs.DONT); 
+                                    tcpSocket.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WONT : (byte)Verbs.DONT);
                                 tcpSocket.GetStream().WriteByte((byte)inputoption);
                                 break;
                             default:
